e28c2aea75b46ae0000df8baf1348ea3a2e90482
Craig Rowe <craig@cargowire.net>
2014-01-18 14:25:09 +0000

    Initial Commit

---
+++ MainPage.xaml.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.Phone.Controls;
+using Microsoft.Phone.Info;
+using System;
+using System.Linq;
+using System.Windows;
+using System.Windows.Navigation;
+using System.Windows.Threading;
+
+namespace PhoneWebBrowserTest
+{
+    public partial class MainPage : PhoneApplicationPage
+    {
+        private DispatcherTimer updateTimer;
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        protected override void OnNavigatedTo(NavigationEventArgs e)
+        {
+            base.OnNavigatedTo(e);
+
+            if (this.updateTimer == null)
+            {
+                this.updateTimer = new DispatcherTimer();
+                this.updateTimer.Interval = TimeSpan.FromMilliseconds(2000);
+                this.updateTimer.Tick += this.OnTimer;
+                this.updateTimer.Start();
+            }
+
+            GC.Collect();
+        }
+
+        protected override void OnNavigatedFrom(NavigationEventArgs e)
+        {
+            this.updateTimer.Tick -= this.OnTimer;
+            this.updateTimer.Stop();
+            this.updateTimer = null;
+        }
+
+        private void OnTimer(object sender, EventArgs e)
+        {
+            this.Memory.Text = (DeviceStatus.ApplicationCurrentMemoryUsage / 1048576.0) + "MB";
+        }
+
+        private void Action_Click(object sender, RoutedEventArgs e)
+        {
+            this.NavigationService.Navigate(new Uri("/WebBrowserPage.xaml", UriKind.Relative));
+        }
+    }
+}
---
+++ WebBrowserPage.xaml.cs
@@ -0,0 +1,71 @@
+﻿using Microsoft.Phone.Controls;
+using Microsoft.Phone.Info;
+using System;
+using System.Windows;
+using System.Windows.Navigation;
+using System.Windows.Threading;
+
+namespace PhoneWebBrowserTest
+{
+    public partial class WebBrowserPage : PhoneApplicationPage
+    {
+        private DispatcherTimer updateTimer;
+
+        public WebBrowserPage()
+        {
+            InitializeComponent();
+        }
+
+        protected override void OnNavigatedFrom(NavigationEventArgs e)
+        {
+            base.OnNavigatedFrom(e);
+
+            this.browser.Navigated -= browser_Navigated;
+            this.updateTimer.Stop();
+            this.updateTimer.Tick -= this.OnTimer;
+            this.updateTimer = null;
+        }
+
+        protected override void OnNavigatedTo(NavigationEventArgs e)
+        {
+            if (this.updateTimer == null)
+            {
+                this.updateTimer = new DispatcherTimer();
+                this.updateTimer.Interval = TimeSpan.FromMilliseconds(2000);
+                this.updateTimer.Tick += this.OnTimer;
+                this.updateTimer.Start();
+
+                this.browser.Navigated += browser_Navigated;
+            }
+        }
+
+        private void browser_Navigated(object sender, NavigationEventArgs e)
+        {
+            browser.ClearInternetCacheAsync();
+            browser.ClearCookiesAsync();
+        }
+
+        private void OnTimer(object sender, EventArgs e)
+        {
+            this.Memory.Text = (DeviceStatus.ApplicationCurrentMemoryUsage / 1048576.0) + "MB";
+        }
+
+        private void Refresh_Click(object sender, RoutedEventArgs e)
+        {
+            browser.InvokeScript("eval", "location.reload();");
+        }
+
+        private void Blank_Click(object sender, RoutedEventArgs e)
+        {
+            browser.Navigate(new Uri("about:blank"));
+            browser.ClearInternetCacheAsync();
+            browser.ClearCookiesAsync();
+        }
+
+        private void Nav_Click(object sender, RoutedEventArgs e)
+        {
+            browser.Navigate(new Uri("about:blank"));
+            this.NavigationService.GoBack();
+        }
+    }
+}
